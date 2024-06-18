22222238651ad79981df891797b0f077f09e84ef
Nopporn.t <132421410+nuynetza00123@users.noreply.github.com>
2024-06-18 10:29:44 +0700

    ss

--- src/api/environment.js
+++ src/api/environment.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: `https://punn-api.fs-omc.io/api/`,
-  baseURL: `https://10.144.66.34/api/`,
+  baseURL: `https://punn-api.fs-omc.io/api/`,
+  // baseURL: `https://10.144.66.34/api/`,
 });
